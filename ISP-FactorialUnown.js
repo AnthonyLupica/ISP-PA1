00ce3add955632f9e94cf99fd5956840a93c9b30
Anthony R. Lupica <arl127@uakron.edu>
2022-09-28 15:32:20 -0400

    Add feature to detect browser

---
+++ ISP-FactorialUnown.js
@@ -0,0 +1,47 @@
+// Javascript file for project 1 - ISP
+
+function whatBrowser() 
+{
+    // idexOf() returns -1 if the string is not found within the user-agent header
+
+    // firefox
+    if (navigator.userAgent.indexOf("Firefox/") != -1 ) 
+    {
+        alert("Browser/version: " + navigator.userAgent.split(" ")[10]); 
+        return;
+    }
+
+    // chrome
+    else if (navigator.userAgent.indexOf("Chrome/") != -1 ) 
+    {
+        alert("Browser/version: " + navigator.userAgent.split(" ")[10]); 
+        return;
+    } 
+
+    // internet explorer
+    else if (navigator.userAgent.indexOf("MSIE/") != -1 ) 
+    {
+        alert("Browser/version: " + navigator.userAgent.split(" ")[10]); 
+        return;
+    } 
+
+    // edge
+    else if (navigator.userAgent.indexOf("Edg/") != -1 ) 
+    {
+        alert("Browser/version: " + navigator.userAgent.split(" ")[10]); 
+        return;
+    } 
+
+    // safari
+    else if (navigator.userAgent.indexOf("Safari/") != -1 ) 
+    {
+        alert("Browser/version: " + navigator.userAgent.split(" ")[10]); 
+        return;
+    } 
+
+    else
+    {
+        alert("Cannot detect browser");
+        return;
+    }
+}
