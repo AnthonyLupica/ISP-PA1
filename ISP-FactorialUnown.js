3c128b94e677926025432c06acb0aa0191b142b5
Anthony R. Lupica <arl127@uakron.edu>
2022-09-28 15:49:53 -0400

    Dates requirement done

--- ISP-FactorialUnown.js
+++ ISP-FactorialUnown.js
@@ -1,41 +1,41 @@
 // Javascript file for project 1 - ISP
 
-function whatBrowser() 
+window.onload = function whatBrowser() 
 {
     // idexOf() returns -1 if the string is not found within the user-agent header
 
     // firefox
     if (navigator.userAgent.indexOf("Firefox/") != -1 ) 
     {
-        alert("Browser/version: " + navigator.userAgent.split(" ")[10]); 
+        alert("Browser/version--> " + navigator.userAgent.split(" ")[10]); 
         return;
     }
 
     // chrome
     else if (navigator.userAgent.indexOf("Chrome/") != -1 ) 
     {
-        alert("Browser/version: " + navigator.userAgent.split(" ")[10]); 
+        alert("Browser/version--> " + navigator.userAgent.split(" ")[10]); 
         return;
     } 
 
     // internet explorer
     else if (navigator.userAgent.indexOf("MSIE/") != -1 ) 
     {
-        alert("Browser/version: " + navigator.userAgent.split(" ")[10]); 
+        alert("Browser/version--> " + navigator.userAgent.split(" ")[10]); 
         return;
     } 
 
     // edge
     else if (navigator.userAgent.indexOf("Edg/") != -1 ) 
     {
-        alert("Browser/version: " + navigator.userAgent.split(" ")[10]); 
+        alert("Browser/version--> " + navigator.userAgent.split(" ")[10]); 
         return;
     } 
 
     // safari
     else if (navigator.userAgent.indexOf("Safari/") != -1 ) 
     {
-        alert("Browser/version: " + navigator.userAgent.split(" ")[10]); 
+        alert("Browser/version--> " + navigator.userAgent.split(" ")[10]); 
         return;
     } 
 
@@ -45,3 +45,4 @@ function whatBrowser()
         return;
     }
 }
+
